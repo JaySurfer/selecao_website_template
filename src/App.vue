708b9519b3a51b7c019c8ce153878579ea8b07cf
Jay Mahole <102528145+JaySurfer@users.noreply.github.com>
2025-05-23 09:58:29 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavSection from './components/NavSection.vue'
+import HeroSection from './components/HeroSection.vue'
+</script>
+
+<template>
+  <div class="bg-gray-800 pb-120">
+    <NavSection />
+    <HeroSection />
+  </div>
+</template>
---
+++ src/components/NavSection.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex justify-center items-center">
+    <ul>
+      <li class="inline-block pt-5 items-center">
+        <a class="font-bold text-3xl pr-40 pt-5 text-white" href="#">Selecao</a>
+        <a
+          class="p-4 px-3 py-1.5 text-white hover:bg-white hover:text-black font-bold rounded-4xl ml-9"
+          href="#"
+          >Home</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 ml-5 font-bold rounded-4xl text-white"
+          href="#"
+          >About</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 p-10 ml-5 rounded-4xl font-bold text-white"
+          href="#"
+          >Services</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 p-10 ml-5 rounded-4xl font-bold text-white"
+          href="#"
+          >Portfolio</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 p-10 ml-5 rounded-4xl font-bold text-white"
+          href="#"
+          >Team</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 p-10 ml-5 rounded-4xl font-bold text-white"
+          href="#"
+          >Blog</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 p-10 ml-5 rounded-4xl font-bold text-white"
+          href="#"
+          >Dropdown</a
+        >
+        <a
+          class="hover:bg-white hover:text-black px-3 py-1.5 p-10 ml-5 rounded-4xl font-bold text-white"
+          href="#"
+          >Contact</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
